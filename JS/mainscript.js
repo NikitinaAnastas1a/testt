2e83fe2ce8a6d4aa9454802718e53440d7821a80
nikitina_a <nikitinanancy@gmail.com>
2025-12-14 11:55:34 +0200

    Add files via upload

---
+++ JS/mainscript.js
@@ -0,0 +1,38 @@
+        // alert("Hello World!");
+        // alert(`This is my first script on JavaScript,
+        //         Hello World!`);
+        // alert(3 + 15 + 48);
+        // [8, 3, 5].forEach(alert)
+        /* Привіт */
+        // let text;
+        // text = 'Hello, World!', count = 5;
+        // alert(text)
+        // alert(count)
+        // let text;
+        // text = 'Hello';
+        // text= 'World';
+        // alert(text);
+
+        // let text = 'Hello, World!';
+        // let message;
+        // message = text;
+        // alert(text);
+        // alert(message);
+        // const MY_NAME = "Анастасія";
+        // const COLOR_GREEN
+        // let user, name;
+        // name = "В'ячеслав";
+        // user = name;
+        // document.write(`<p class="p1">${user}</p>`);
+
+
+
+const planetName = "Земля"; 
+let userName = "Василь";
+
+console.log("--- Вивід змінних ---");
+console.log("Змінна для планети:");
+console.log(`Ім'я змінної: planetName, Значення: ${planetName}`);
+
+console.log("Змінна для користувача:");
+console.log(`Ім'я змінної: userName, Значення: ${userName}`);
